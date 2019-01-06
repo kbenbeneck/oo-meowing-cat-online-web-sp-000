169ca9d1d2b4adc6431c2bcbd286fdcd3ebe2266
Karl Benbeneck <kbenbeneck@gmail.com>
2019-01-06 01:16:46 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,7 @@
 ## code your solution here.
 class Cat
 
-  def name=(cats_name)
-    @this_cats_name = cats_name
-  end
-
-  def name
-    @this_cats_name
-  end
+  attr_reader :name
+  attr_writer :name
+  
 end
